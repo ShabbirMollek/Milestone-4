be98d225b4b81ab7fc36dda4e17de405cf3b651f
Sabbirador <sabbirador69@gmail.com>
2024-01-27 01:53:27 +0600

    some problem solving

--- first.js
+++ first.js
@@ -1,1 +1,55 @@
-console.log(89)
+// problem -1
+
+var myMoney = 1000;
+var orange =450;
+var apple =250;
+
+var totalCoast = orange + apple ;
+// console.log(totalCoast);
+var iHave = myMoney - totalCoast;
+// console.log(iHave);
+
+
+// problem -2
+
+var matheMatics = 75.25;
+var biology =65;
+var Chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalNumber=matheMatics+biology+Chemistry+physics+bangla;
+// console.log(totalNumber);
+
+var average = totalNumber / 5;
+// console.log(average.toFixed(2));    
+
+// for expected value we are using .toFixed after fraction number
+
+// problem -3
+
+var sampleValue =119;
+var number = 5;
+
+var output = sampleValue % number ;
+
+// console.log(output);
+
+// its called reminder 
+
+// problem -4 
+
+var a = isNaN('11');
+
+// console.log(a);
+
+
+// is NaN means is it Number 
+
+var a=isNaN(2-10);
+
+// console.log(a);
+
+
+var a=isNaN(10);
+console.log(a);
