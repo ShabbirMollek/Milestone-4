5766bc648422155251158817e09bd8205de088d1
Sabbirador <sabbirador69@gmail.com>
2024-01-27 18:17:22 +0600

    multiple.js

---
+++ multiple.js
@@ -0,0 +1,41 @@
+
+var isBcs =false;
+var salary = 35000;
+var hasCar= true;
+var age =32;
+
+// or condition
+
+// if (isBcs == true || salary >30000){
+//                 console.log(" I will marry you")
+// }
+// else {
+//                 console.log("next time")
+// }
+
+// and condition
+
+// if (isBcs == true && salary >30000){
+//                 console.log(" I will marry you")
+// }
+// else {
+//                 console.log("next time")
+// }
+
+// complex condition and or both applying
+
+// if ((hasCar == true && salary >30000) || age >=28  ) {
+//                 console.log(" I will marry you")
+// }
+// else {
+//                 console.log("next time")
+// }
+
+// 
+
+if ((hasCar == true || salary > 400000) && age == 28  ) {
+              console.log(" I will marry you")
+}
+ else {
+                console.log("next time")
+}
