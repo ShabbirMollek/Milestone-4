1c48ddece70c3772944d3c3764dbb4edf8ce00a9
Sabbirador <sabbirador69@gmail.com>
2024-01-27 17:52:41 +0600

    comparison js

---
+++ comparison.js
@@ -0,0 +1,18 @@
+if (45>50){
+                // console.log("its Odd number")
+}
+else{
+                // console.log("its even number")
+}
+
+// ...............
+
+var guavaPrice = 60 ;
+var applePrice =160;
+
+if (applePrice>guavaPrice){
+                // console.log("i buy Guava")
+}
+else(
+//                 console.log("I have no enough money")
+// )
