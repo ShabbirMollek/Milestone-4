c9d1369ad68cc236b0fe82906a77a9004cefbfff
Sabbirador <sabbirador69@gmail.com>
2024-01-30 02:36:30 +0600

    for loop and while loop

---
+++ for-loop.js
@@ -0,0 +1,23 @@
+
+for (let i = 0 ; i < 10 ; i ++ ){
+                // console.log(i);
+}
+
+// for even number 
+for (let i = 0 ; i <=20 ; i=i+2 ){
+                // console.log(i);
+}
+// for odd number
+for (let i = 1 ; i <=20 ; i=i+2 ){
+                // console.log(i);
+}
+
+
+let sum = 0 ;
+
+for (let i = 11 ; i <= 15 ; i++){
+                sum = sum + i ;
+                // console.log(i);
+              
+}
+// console.log('total number of 11 to 15 is:' ,sum);
