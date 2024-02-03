adf0fe96390c3ff0fb6ead32070c6224932cfc0f
Sabbirador <sabbirador69@gmail.com>
2024-02-03 19:59:51 +0600

    simple coding problem part-1

---
+++ return.js
@@ -0,0 +1,11 @@
+
+// function bigNumber(number){
+//          const bigNumber=  number*100 ;
+//          return bigNumber ;     
+// }
+// const output = bigNumber(50);
+// console.log("Output is :" , output);
+
+function getMenu() {
+                console.log("Burger, Pizza, Pasta");
+               }
