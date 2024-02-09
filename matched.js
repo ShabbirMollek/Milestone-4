8435d680818a4ae01c6b3498e1dbdf3756c46b10
Sabbirador <sabbirador69@gmail.com>
2024-02-09 20:04:15 +0600

    milestone four almost done

---
+++ matched.js
@@ -0,0 +1,20 @@
+function add (x,y){
+                if (typeof x !== 'number' || typeof y !=='number'){
+                                return "Invalid OutPut" ;
+                }
+                return x + y ;
+}
+
+console.log(add(4,53));
+
+
+
+
+function add(x,y){
+                if (typeof x!== 'number' || typeof y!== 'number'){
+                                return 'Thik thak number de'
+                }
+                return x+ y ;
+}
+console.log(add(89,67))
+
---
+++ simple-problem-2.js
@@ -0,0 +1,21 @@
+const student = {
+                name: 'shakib khan' ,
+                id: 121 ,
+                address :'movie cinema' ,
+                isSingle: true ,
+                friends :['apu','raaz','Salman','aamir'],
+                car:{
+                                brand:'tesla',
+                                price:5000000,
+                                made:2004 ,
+                                manufacture :{
+                                                name:'tesla' ,
+                                                ceo:'elon mask',
+                                                country:'USA'
+                                }
+                }
+}
+
+console.log(student.friends);
+console.log(student.car.manufacture);
+console.log(student.isSingle);
